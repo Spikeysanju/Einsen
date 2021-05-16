90bc57d77a9e11b93e6e069aeb65a9c2efd3548e
Spikeysanju <spikeysanju98@gmail.com>
2021-05-16 13:43:16 +0530

    Add Color.kt & Setup Theme.kt #1
    Signed-off-by: Spikeysanju <spikeysanju98@gmail.com>

--- app/src/main/java/dev/spikeysanju/einsen/ui/theme/Color.kt
+++ app/src/main/java/dev/spikeysanju/einsen/ui/theme/Color.kt
@@ -6,3 +6,10 @@ val Purple200 = Color(0xFFBB86FC)
 val Purple500 = Color(0xFF6200EE)
 val Purple700 = Color(0xFF3700B3)
 val Teal200 = Color(0xFF03DAC5)
+
+// green theme
+val green100 = Color(0xFF065A60)
+val green200 = Color(0xFF0B525B)
+val green300 = Color(0xFF144552)
+val green400 = Color(0xFF1B3A4B)
+val green500 = Color(0xFF212F45)
