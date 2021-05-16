a77f369baf6b1654a736af4e13629d13db6d8214
Spikeysanju <spikeysanju98@gmail.com>
2021-05-16 13:48:37 +0530

    Add Fonts | Setup Type.kt #1
    Signed-off-by: Spikeysanju <spikeysanju98@gmail.com>

--- app/src/main/java/dev/spikeysanju/einsen/ui/theme/Theme.kt
+++ app/src/main/java/dev/spikeysanju/einsen/ui/theme/Theme.kt
@@ -37,7 +37,7 @@ fun EinsenTheme(darkTheme: Boolean = isSystemInDarkTheme(), content: @Composable
 
     MaterialTheme(
         colors = colors,
-        typography = Typography,
+        typography = typography,
         shapes = Shapes,
         content = content
     )
