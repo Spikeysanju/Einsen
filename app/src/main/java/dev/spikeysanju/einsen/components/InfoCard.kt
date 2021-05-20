d581a2f7fdb6b418bdfe88bbed14480b94f1845e
Sanju S <spikeysanju98@gmail.com>
2021-05-20 05:52:16 +0530

    Add InfoCard.kt
    Signed-off-by: Spikeysanju <spikeysanju98@gmail.com>

---
+++ app/src/main/java/dev/spikeysanju/einsen/components/InfoCard.kt
@@ -0,0 +1,59 @@
+package dev.spikeysanju.einsen.components
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.wrapContentWidth
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.MaterialTheme.colors
+import androidx.compose.material.MaterialTheme.typography
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun InfoCard(title: String, value: String) {
+    Box(
+        modifier = Modifier
+            .size(90.dp)
+            .clip(RoundedCornerShape(12.dp))
+            .background(color = colors.secondary)
+            .padding(12.dp),
+        contentAlignment = Alignment.Center
+    ) {
+        Column(
+            verticalArrangement = Arrangement.Center,
+            modifier = Modifier.wrapContentWidth()
+        ) {
+            Text(
+                text = value,
+                modifier = Modifier.fillMaxWidth(),
+                color = colors.onPrimary,
+                style = typography.subtitle1,
+                fontWeight = FontWeight.W600,
+                textAlign = TextAlign.Center
+            )
+
+            Spacer(modifier = Modifier.height(4.dp))
+
+            Text(
+                text = title,
+                modifier = Modifier.fillMaxWidth(),
+                color = colors.onPrimary.copy(.7f),
+                style = typography.overline,
+                textAlign = TextAlign.Center
+            )
+        }
+    }
+}
--- app/src/main/java/dev/spikeysanju/einsen/view/details/TaskDetailScreen.kt
+++ app/src/main/java/dev/spikeysanju/einsen/view/details/TaskDetailScreen.kt
@@ -1,17 +1,52 @@
 package dev.spikeysanju.einsen.view.details
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.MaterialTheme.colors
 import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.text.style.TextAlign
 import androidx.navigation.NavHostController
 import dev.spikeysanju.einsen.components.TopBarWithBack
 import dev.spikeysanju.einsen.navigation.MainActions
+import dev.spikeysanju.einsen.ui.theme.typography
 
 @Composable
 fun TaskDetailsScreen(navController: NavHostController, action: MainActions, param: Any) {
     Scaffold(topBar = {
         TopBarWithBack(title = "Task Details", action.upPress)
     }) {
-        Text(text = "Task Details")
+        LazyColumn {
+
+            item {
+                Text(text = "Open Source")
+            }
+
+        }
+    }
+}
+
+@Composable
+fun DetailsTextView() {
+    Column {
+        Text(
+            text = "Open Source",
+            style = typography.subtitle1,
+            textAlign = TextAlign.Start,
+            color = colors.onPrimary
+        )
+        Text(
+            text = "Your Title here",
+            style = typography.h5,
+            textAlign = TextAlign.Start,
+            color = colors.onPrimary
+        )
+        Text(
+            text = "Your description goes here...",
+            style = typography.body1,
+            textAlign = TextAlign.Start,
+            color = colors.onPrimary
+        )
     }
 }
