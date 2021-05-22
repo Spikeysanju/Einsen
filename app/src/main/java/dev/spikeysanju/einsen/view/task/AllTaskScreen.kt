ff0a564931d8c43e52126ec80b031b2468c27bfa
Sanju S <spikeysanju98@gmail.com>
2021-05-22 14:56:01 +0530

    Complete AllTaskScreen.kt UI
    Signed-off-by: Spikeysanju <spikeysanju98@gmail.com>

--- app/src/main/java/dev/spikeysanju/einsen/view/task/AllTaskScreen.kt
+++ app/src/main/java/dev/spikeysanju/einsen/view/task/AllTaskScreen.kt
@@ -1,9 +1,13 @@
 package dev.spikeysanju.einsen.view.task
 
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.Scaffold
-import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
+import dev.spikeysanju.einsen.components.TaskItemCard
 import dev.spikeysanju.einsen.components.TopBarWithBack
 import dev.spikeysanju.einsen.navigation.MainActions
 import dev.spikeysanju.einsen.view.viewmodel.MainViewModel
@@ -17,6 +21,17 @@ fun AllTaskScreen(
     Scaffold(topBar = {
         TopBarWithBack(title = "All Task", actions.upPress)
     }) {
-        Text(text = "All Task")
+
+        LazyColumn(modifier = Modifier.padding(start = 16.dp, end = 16.dp)) {
+            items(10) {
+                TaskItemCard(
+                    id = "1",
+                    title = "UX Research",
+                    emoji = "📮",
+                    category = "Open source",
+                    timer = "15:00"
+                )
+            }
+        }
     }
 }
