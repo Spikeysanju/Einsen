3b34c7e580e18dd857f37e70d8224446fb62941a
Sanju S <spikeysanju98@gmail.com>
2021-05-27 16:14:39 +0530

    Add DI Module | Complete Room Setup #17
    Signed-off-by: Spikeysanju <spikeysanju98@gmail.com>

--- app/src/main/java/dev/spikeysanju/einsen/data/datastore/db/EinsenDatabase.kt
+++ app/src/main/java/dev/spikeysanju/einsen/data/datastore/db/EinsenDatabase.kt
@@ -1,9 +1,10 @@
 package dev.spikeysanju.einsen.data.datastore.db
 
 import androidx.room.Database
+import androidx.room.RoomDatabase
 import dev.spikeysanju.einsen.model.Task
 
 @Database(entities = [Task::class], version = 1, exportSchema = false)
-abstract class EinsenDatabase {
+abstract class EinsenDatabase : RoomDatabase() {
     abstract fun getTaskDao(): TaskDao
 }
