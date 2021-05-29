9baddcf24d81386535746be386a2f1c9f4ef298b
Sanju S <spikeysanju98@gmail.com>
2021-05-29 20:48:42 +0530

    Implement All Task
    Signed-off-by: Spikeysanju <spikeysanju98@gmail.com>

--- app/src/main/java/dev/spikeysanju/einsen/navigation/Screen.kt
+++ app/src/main/java/dev/spikeysanju/einsen/navigation/Screen.kt
@@ -6,7 +6,7 @@ import dev.spikeysanju.einsen.R
 sealed class Screen(val route: String, @StringRes val resourceId: Int) {
     object Home : Screen("home", R.string.text_home)
     object AddTask : Screen("add_task", R.string.text_addTask)
-    object AllTask : Screen("settings", R.string.text_allTask)
+    object AllTask : Screen("all_task", R.string.text_allTask)
     object TaskDetails : Screen("details", R.string.text_taskDetails)
     object Settings : Screen("settings", R.string.text_settings)
 }
--- app/src/main/java/dev/spikeysanju/einsen/view/task/AllTaskScreen.kt
+++ app/src/main/java/dev/spikeysanju/einsen/view/task/AllTaskScreen.kt
@@ -2,14 +2,17 @@ package dev.spikeysanju.einsen.view.task
 
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
 import dev.spikeysanju.einsen.components.TaskItemCard
 import dev.spikeysanju.einsen.components.TopBarWithBack
 import dev.spikeysanju.einsen.navigation.MainActions
+import dev.spikeysanju.einsen.utils.ViewState
 import dev.spikeysanju.einsen.view.viewmodel.MainViewModel
 
 @Composable
@@ -22,15 +25,27 @@ fun AllTaskScreen(
         TopBarWithBack(title = "All Task", actions.upPress)
     }) {
 
-        LazyColumn(modifier = Modifier.padding(start = 16.dp, end = 16.dp)) {
-            items(10) {
-                TaskItemCard(
-                    id = "1",
-                    title = "UX Research",
-                    emoji = "📮",
-                    category = "Open source",
-                    timer = "15:00"
-                )
+        viewModel.getAllTask()
+
+        when (val result = viewModel.feed.collectAsState().value) {
+            ViewState.Loading -> {
+            }
+            ViewState.Empty -> {
+            }
+            is ViewState.Success -> {
+                LazyColumn(modifier = Modifier.padding(start = 16.dp, end = 16.dp)) {
+                    items(result.task) { task ->
+                        TaskItemCard(
+                            id = task.id.toString(),
+                            title = task.title,
+                            emoji = "📮",
+                            category = task.category,
+                            timer = task.timer.toString()
+                        )
+                    }
+                }
+            }
+            is ViewState.Error -> {
             }
         }
     }
