c5bac2a0720bbe4e0a093ce242df8e6303d91eb0
Sanju S <spikeysanju98@gmail.com>
2021-06-07 13:43:51 +0530

    Complete EmojiList ViewModel Logic
    Signed-off-by: Spikeysanju <spikeysanju98@gmail.com>

--- app/src/main/java/dev/spikeysanju/einsen/view/viewmodel/MainViewModel.kt
+++ app/src/main/java/dev/spikeysanju/einsen/view/viewmodel/MainViewModel.kt
@@ -3,12 +3,15 @@ package dev.spikeysanju.einsen.view.viewmodel
 import android.content.Context
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.squareup.moshi.JsonAdapter
 import com.squareup.moshi.Moshi
 import com.squareup.moshi.Types
 import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
 import dagger.hilt.android.lifecycle.HiltViewModel
+import dev.spikeysanju.einsen.model.EmojiItem
 import dev.spikeysanju.einsen.model.Task
 import dev.spikeysanju.einsen.repository.MainRepository
+import dev.spikeysanju.einsen.utils.EmojiViewState
 import dev.spikeysanju.einsen.utils.SingleViewState
 import dev.spikeysanju.einsen.utils.ViewState
 import kotlinx.coroutines.Dispatchers
@@ -25,10 +28,12 @@ class MainViewModel @Inject constructor(private val repo: MainRepository) : View
     // Backing property to avoid state updates from other classes
     private val _viewState = MutableStateFlow<ViewState>(ViewState.Loading)
     private val _singleViewState = MutableStateFlow<SingleViewState>(SingleViewState.Loading)
+    private val _emojiViewState = MutableStateFlow<EmojiViewState>(EmojiViewState.Loading)
 
     // The UI collects from this StateFlow to get its state update
     val feed = _viewState.asStateFlow()
     val singleTask = _singleViewState.asStateFlow()
+    val emoji = _emojiViewState.asStateFlow()
 
     // get all task
     fun getAllTask() = viewModelScope.launch(Dispatchers.IO) {
@@ -53,9 +58,21 @@ class MainViewModel @Inject constructor(private val repo: MainRepository) : View
             val moshi = Moshi.Builder()
                 .add(KotlinJsonAdapterFactory())
                 .build()
-            val listType = Types.newParameterizedType(List::class.java)
-        } catch (e: Exception) {
+            val listType = Types.newParameterizedType(List::class.java, EmojiItem::class.java)
+            val adapter: JsonAdapter<List<EmojiItem>> = moshi.adapter(listType)
+            val myJson = context.assets.open("allEmoji.json").bufferedReader().use {
+                it.readText()
+            }
 
+            val emojiList = adapter.fromJson(myJson)
+            if (emojiList.isNullOrEmpty()) {
+                _emojiViewState.value = EmojiViewState.Empty
+            } else {
+                _emojiViewState.value = EmojiViewState.Success(emojiItem = emojiList)
+            }
+
+        } catch (e: Exception) {
+            _emojiViewState.value = EmojiViewState.Error(exception = e)
         }
     }
 
